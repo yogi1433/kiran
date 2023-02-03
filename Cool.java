83e38f88749284c14b6180d3bc02e5012fe09cfd
yogi1433 <parnapalliyogeshhanuman@gmail.com>
2023-02-03 17:01:31 +0530

    home work by kiran sir

---
+++ Cool.java
@@ -0,0 +1,15 @@
+class Cool 
+{
+public static void main(String[] args)
+{
+int n=3;
+if(n>=1 && n<=4)
+System.out.println("Its Summer");
+else if (n>=5 && n<=10)
+System.out.println("Its Rainny");
+else if (n>=11 && n<=12)
+System.out.println("Its Winter");
+else
+System.out.println("Invalid");
+}
+}
